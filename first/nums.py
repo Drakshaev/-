37f66177a00a00c2f6b2f85714f0129ccb06d332
Дмитрий Ракшаев <dima.rakshaev@mail.ru>
2022-12-14 17:19:23 +0300

    добавль экземпляр класса "наставник"

--- first/nums.py
+++ first/nums.py
@@ -3,3 +3,12 @@ class Student:
         self.name = name
         self.surname = surname
         self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+class Mentor:
+    def __int__(self,name,surname,):
+        self.name = name
+        self.surname = surname
+        self.courses_attached
